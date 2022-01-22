69ad9101d18feaeddc1d8571f39a2f3fc6392562
AurelTBE <aureltrouble@gmail.com>
2022-01-22 16:49:27 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import Technologies from '../components/Technologies';
+import Timeline from '../components/Timeline';
+
+function App() {
+
+  return (
+    <div>
+      <Hero />
+      <Projects />
+      <Technologies />
+      <Timeline />
+    </div>
+  )
+}
+
+export default App
